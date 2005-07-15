070e6d3ea109248774c8e3a6cc64f65ce3e01beb
christian <christian@b5c8ebf9-7d75-0410-a1b1-c26d7bff6aa0>
2005-07-15 12:14:40 +0000

    - added some lines of code for tui

--- trunk/src/applicator/startup/startup_linux_console.c
+++ trunk/src/applicator/startup/startup_linux_console.c
@@ -20,7 +20,7 @@
  * http://www.cybop.net
  * - Cybernetics Oriented Programming -
  *
- * @version $Revision: 1.3 $ $Date: 2005-07-14 17:41:44 $ $Author: christian $
+ * @version $Revision: 1.4 $ $Date: 2005-07-15 12:14:40 $ $Author: christian $
  * @author Christian Heller <christian.heller@tuxtax.de>
  * @description
  */
@@ -28,11 +28,20 @@
 #ifndef STARTUP_LINUX_CONSOLE_SOURCE
 #define STARTUP_LINUX_CONSOLE_SOURCE
 
+#include <sys/ioctl.h>
+#include <sys/vt.h>
+//?? #include <sys/stat.h>
+//?? #include <sys/types.h>
+//?? #include <sys/wait.h>
+//?? #include <stdio.h>
+//?? #include <unistd.h>
+//?? #include <stdlib.h>
+//?? #include <signal.h>
+#include <fcntl.h>
+#include "../../memoriser/creator/integer_creator.c"
 #include "../../globals/constants/integer_constants.c"
 #include "../../globals/constants/structure_constants.c"
 #include "../../globals/variables/variables.c"
-#include "../../memoriser/creator/integer_creator.c"
-#include "../../memoriser/creator/unsigned_long_creator.c"
 
 /**
  * Starts up the linux console.
@@ -60,22 +69,61 @@ void startup_linux_console(void* p0, const void* p1, const void* p2, const void*
         // Create linux console internals.
         create_integer((void*) &t);
 
-        // Initialise linux console internals.
-        if (strcmp("linux", getenv("TERM")) == 0) {
+        // Check for linux console.
+        int l = strcmp("linux", getenv("TERM"));
 
-            log_message_debug("TEST: This is a linux system.");
+        if (l == 0) {
 
-            *t = ttyname();
-
-            fprintf(stdout, "TEST: The terminal device name is: %i\n", *t);
-
-            // Set linux console internals.
-            set_array_elements(p0, (void*) LINUX_CONSOLE_TERMINAL_INTERNAL, (void*) &t, (void*) ONE_INTEGER, (void*) POINTER_ARRAY);
+            log_message_debug("TEST: This is a linux console.");
 
         } else {
 
-            log_message_debug("ERROR: Could not startup linux console. This is not a linux system.");
+            log_message_debug("TEST: This is a standard serial terminal.");
         }
+
+        // Open virtual console.
+//??        *t = ttyname();
+        *t = open("/dev/tty", O_RDWR, 0);
+
+        if (*t >= 0) {
+
+            // Check for virtual console.
+            struct vt_stat vtstat;
+            int v = ioctl(*t, VT_GETSTATE, &vtstat);
+
+            if (v >= 0) {
+
+                // Check for free virtual console.
+                int vtnum;
+                int f = ioctl(*t, VT_OPENQRY, &vtnum);
+
+                if (f >= 0) {
+
+/*??
+                    // The device name.
+                    char* d = create_array...
+                    sprintf(d, ());
+*/
+
+                } else {
+
+                    log_message_debug("ERROR: Could not startup linux console. There are no free virtual consoles.");
+                }
+
+            } else {
+
+                log_message_debug("ERROR: Could not startup linux console. The tty device is not a virtual console.");
+            }
+
+        } else {
+
+            log_message_debug("ERROR: Could not startup linux console. The virtual terminal could not be opened.");
+        }
+
+        fprintf(stdout, "TEST: The terminal device name is: %i\n", *t);
+
+        // Set linux console internals.
+        set_array_elements(p0, (void*) LINUX_CONSOLE_TERMINAL_INTERNAL, (void*) &t, (void*) ONE_INTEGER, (void*) POINTER_ARRAY);
 
     } else {
 
