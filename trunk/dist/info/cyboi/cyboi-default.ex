5129ca2f2ccef7c7fc7293892827abc2d49c6d40
christian <christian@b5c8ebf9-7d75-0410-a1b1-c26d7bff6aa0>
2005-08-30 23:02:51 +0000

    - updated debian package information

---
+++ trunk/dist/info/cyboi/cyboi-default.ex
@@ -0,0 +1,10 @@
+# Defaults for cyboi initscript
+# sourced by /etc/init.d/cyboi
+# installed at /etc/default/cyboi by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
