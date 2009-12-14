f3285fd35ce66ede1a43c5893ee1a995fa72952c
christian <christian@b5c8ebf9-7d75-0410-a1b1-c26d7bff6aa0>
2009-12-14 15:19:34 +0000

    2009-12-14  Christian Heller  <christian.heller@tuxtax.de>         * Split file "assigner.c" and put resulting files into "executor/accessor/assigner/" directory

---
+++ trunk/src/executor/accessor/assigner/integer_assigner.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 1999-2009. Christian Heller.
+ *
+ * This file is part of the Cybernetics Oriented Interpreter (CYBOI).
+ *
+ * CYBOI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CYBOI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CYBOI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Cybernetics Oriented Programming (CYBOP) <http://www.cybop.org>
+ * Christian Heller <christian.heller@tuxtax.de>
+ *
+ * @version $RCSfile: assigner.c,v $ $Revision: 1.1 $ $Date: 2009-10-06 21:25:26 $ $Author: christian $
+ * @author Christian Heller <christian.heller@tuxtax.de>
+ */
+
+#ifndef INTEGER_ASSIGNER_SOURCE
+#define INTEGER_ASSIGNER_SOURCE
+
+#include <stdlib.h>
+#include <string.h>
+#include "../../constant/model/memory/integer_memory_model.c"
+#include "../../constant/model/log/message_log_model.c"
+#include "../../constant/model/memory/pointer_memory_model.c"
+#include "../../logger/logger.c"
+#include "../../variable/primitive_type_size.c"
+
+/**
+ * Assigns the integer.
+ *
+ * @param p0 the destination value
+ * @param p1 the source value
+ */
+void assign_integer(void* p0, void* p1) {
+
+    if (p1 != *NULL_POINTER_MEMORY_MODEL) {
+
+        int* se = (int*) p1;
+
+        if (p0 != *NULL_POINTER_MEMORY_MODEL) {
+
+            int* de = (int*) p0;
+
+            log_terminated_message((void*) DEBUG_LEVEL_LOG_MODEL, (void*) L"Assign integer.");
+
+            // Assign source- to destination value.
+            *de = *se;
+
+        } else {
+
+            log_terminated_message((void*) ERROR_LEVEL_LOG_MODEL, (void*) L"Could not assign integer. The destination value is null.");
+        }
+
+    } else {
+
+        log_terminated_message((void*) ERROR_LEVEL_LOG_MODEL, (void*) L"Could not assign integer. The source value is null.");
+    }
+}
+
+/* INTEGER_ASSIGNER_SOURCE */
+#endif
