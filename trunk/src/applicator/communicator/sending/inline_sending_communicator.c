10a915bf28e0c3e67088c32c38fde9899fcd9c7e
christian <christian@b5c8ebf9-7d75-0410-a1b1-c26d7bff6aa0>
2011-01-07 13:52:07 +0000

    * Prepare http response encoding

--- trunk/src/applicator/communicator/sending/inline_sending_communicator.c
+++ trunk/src/applicator/communicator/sending/inline_sending_communicator.c
@@ -67,8 +67,14 @@ void communicate_sending_inline(void* p0, void* p1, void* p2, void* p3, void* p4
     // Encode source knowledge model into array.
     encode((void*) &a, ac, as, *NULL_POINTER_MEMORY_MODEL, *NULL_POINTER_MEMORY_MODEL, p3, p4, p5, p6, p7, p8, *NULL_POINTER_MEMORY_MODEL, *NULL_POINTER_MEMORY_MODEL, p9, p10);
 
+    fwprintf(stdout, L"TEST sending inline a: %ls\n", (wchar_t*) a);
+    fwprintf(stdout, L"TEST sending inline ac: %i\n", *((int*) ac));
+
     // Write encoded array into destination array.
     send_data(p0, p1, p2, a, ac, (void*) INLINE_CYBOL_CHANNEL, (void*) INLINE_CYBOL_CHANNEL_COUNT);
+
+    fwprintf(stdout, L"TEST sending inline p0: %ls\n", (wchar_t*) p0);
+    fwprintf(stdout, L"TEST sending inline p1: %i\n", *((int*) p1));
 
     // Deallocate array.
     deallocate_model((void*) &a, (void*) &ac, (void*) &as, (void*) NUMBER_0_INTEGER_MEMORY_MODEL, (void*) WIDE_CHARACTER_MEMORY_ABSTRACTION, (void*) WIDE_CHARACTER_MEMORY_ABSTRACTION_COUNT);
